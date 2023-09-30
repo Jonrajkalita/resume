69291fe7a5d80bfb40222d61a686b3a8200c771d
Jonrajkalita <146457714+Jonrajkalita@users.noreply.github.com>
2023-09-30 13:23:11 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,9 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxeJ4Si0QChB3n2C3Ndxd3BOVLsm92lzNxOuuUsHwNaAElaQ-93hn31reRda6Fd4VhL/exec'
-
 const form = document.forms['google-sheet']
 
-form.addEventListener('Submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, {method: 'post', body: new formData(form)})
-      .then(response => alert("Thanks for Submiting Data...."))
-      .catch(error => console.error('Error!', error.message))
-
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thanks for Submiting Detailes...."))
+  .catch(error => console.error('Error', error.message))
 })
